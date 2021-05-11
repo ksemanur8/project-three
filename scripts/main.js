b7008545e0cf5a2139d62d837431461dc104e02a
ksemanur <59751315+ksemanur8@users.noreply.github.com>
2021-05-11 10:47:57 -0400

    Set up divs, placement of elements in 1st div done

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+window.onload(function () {
+    setTimeout(function(){
+        var welcome = document.getElementById("#welcome");
+        welcome.style.backgroundColor("black");
+    }, 20);
+});
