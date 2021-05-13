9325de9031c11110ea7fe136658f8e7b6a7024b1
ksemanur <59751315+ksemanur8@users.noreply.github.com>
2021-05-13 15:30:42 -0400

    Intersection observer implemented

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
+/* Injecting name input into spans */
 let nameButton = document.getElementById("name-submit");
 
 function injectName() {
   let name = document.getElementById("name-input").value;
-  
   let spans = document.querySelectorAll("span.name-blank");
   
   spans.forEach((span) => {
@@ -12,29 +12,50 @@ function injectName() {
 
 nameButton.addEventListener("click", injectName);
 
-function bubbleColor(destination) {
-    let bubbleTo = destination;
-    if(bubbleTo == "ralph-hello" || bubbleTo == "introduction"|| bubbleTo == "game" || bubbleTo == "how-to-help") {
-        let bubbles = document.querySelectorAll('div.bubble');
-        for (let i = 0; i < bubbles.length; i++) {
-            bubbles[i].classList.remove('light-bubble');
-            bubbles[i].classList.add('dark-bubble');
-        }
-        let labels = document.querySelectorAll('div.label');
-        for (let i = 0; i < labels.length; i++) {
-            labels[i].classList.remove('light-label');
-            labels[i].classList.add('dark-label');
-        }
-    } else if(bubbleTo == "welcome" || bubbleTo == "sources") {
-        let bubbles = document.querySelectorAll('div.bubble');
-        for (let i = 0; i < bubbles.length; i++) {
-            bubbles[i].classList.remove('dark-bubble');
-            bubbles[i].classList.add('light-bubble');
-        }
-        let labels = document.querySelectorAll('div.label');
-        for (let i = 0; i < labels.length; i++) {
-            labels[i].classList.remove('dark-label');
-            labels[i].classList.add('light-label');
-        }
-    }
+/* Intersection observer for changing bubble nav color! */
+let options = {
+    root: null,
+    threshold: 0.5,
 }
+
+function intersectionHandler(entries, observer) {
+    let bubbles = document.querySelectorAll('div.bubble');
+    let labels = document.querySelectorAll('div.label');
+
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+           if(entry.target.classList.contains('light')) {
+                for (let i = 0; i < bubbles.length; i++) {
+                    bubbles[i].classList.remove('light-bubble');
+                    bubbles[i].classList.add('dark-bubble');
+                }
+                for (let i = 0; i < labels.length; i++) {
+                    labels[i].classList.remove('light-label');
+                    labels[i].classList.add('dark-label');
+                }
+           } else if (entry.target.classList.contains('dark')) {
+                for (let i = 0; i < bubbles.length; i++) {
+                    bubbles[i].classList.remove('dark-bubble');
+                    bubbles[i].classList.add('light-bubble');
+                }
+                for (let i = 0; i < labels.length; i++) {
+                    labels[i].classList.remove('dark-label');
+                    labels[i].classList.add('light-label');
+                }
+           }
+        }
+    });
+}
+
+let observer = new IntersectionObserver(intersectionHandler, options);
+/*
+    We need to get reference to all of our slides
+*/
+let slides = document.querySelectorAll('div.slide');
+/* 
+    Loop through all slides and tell the observer to
+    observe each one
+*/
+slides.forEach((slide) => {
+    observer.observe(slide);
+});
